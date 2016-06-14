b8a6497b6f52a852c1f47feb09759b46d4721ccc
Navjot Sagoo <nsagoo@pivotal.io>
2016-06-14 14:33:38 -0400

    add manifest.yaml

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -6,3 +6,5 @@ var LatestEarthquakes = require('latesteqs/LatestEarthquakes');
 LatestEarthquakes({
   el: document.querySelector('#latest-earthquakes')
 });
+
+LatestEarthquake.listen(process.env.PORT || 4000)
